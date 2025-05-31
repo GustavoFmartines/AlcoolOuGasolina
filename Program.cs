0667f3f38ef970133ad3def96065d1f18220096b
GustavoFmartines <gustavo.martines2003@outlook.com>
2025-05-31 15:42:54 -0300

    programa que faz a função de comparar e recomendar se vale a penas abastercer com gasolina ou álcool.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
 ﻿Console.Clear();
 
+Console.WriteLine("---Super comparador de abastecimento---");
+
+Console.WriteLine("Digite o valor do Alcool: ");
+    decimal alcool = Convert.ToDecimal(Console.ReadLine()!);
+
+Console.WriteLine("Digite o valor da Gasolina: ");
+    decimal gasolina = Convert.ToDecimal(Console.ReadLine()!);
+
+    decimal razao = CalcularRazao(alcool, gasolina);
+    decimal percentual = razao * 100;
+
+Console.WriteLine($"\nOpreço do álcool corresponde a {percentual:F1}% do preço da gasolina.");
+
+if (ValeAPenaGasolina(razao))
+    Console.WriteLine("\nRecomendação: Abasteça com Gasolina amigo!");
+else
+    Console.WriteLine("\nRecomendação: Abasteça com Álcool amigo!");
+
+decimal CalcularRazao(decimal alcool, decimal gasolina)
+{
+    return alcool / gasolina;
+}
+
+bool ValeAPenaGasolina(decimal razao)
+{
+    return razao > 0.73m;
+}
